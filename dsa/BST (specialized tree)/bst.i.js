f47243b203ee5951b41ffc230c140533e1133869
sivin-wi <sivinwilson02@gmail.com>
2025-06-23 17:19:54 +0530

    update bst algorithm

--- dsa/BST (specialized tree)/bst.i.js
+++ dsa/BST (specialized tree)/bst.i.js
@@ -1,8 +1,10 @@
-class Node{
-    constructor(value){
-        this.value=value
-        this.left=null
-        this.right=null
+// https://youtu.be/5cU1ILGy6dM?list=PL_aHnKR6K2T-HAk5zE4w6Sy_NTLa1xngW&t=408
+
+class  TreeNode{
+    constructor(val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
     }
 }
 
@@ -10,125 +12,194 @@ class BST{
     constructor(){
         this.root=null
     }
-    _min(node){
-        if(!node) return null
-        if(node.left){
-          return  this._min(node.left)
+    add(val){
+     
+        const node = this.root;
+        if(node===null){
+            this.root = new TreeNode(val)
+            return;
+        }else{
+           const searchTree  = (node)=>{
+              if(val<node.val){
+                 if(node.left===null){
+                     node.left = new TreeNode(val)
+                     return;
+                 }else{
+                      return  searchTree(node.left)
+                 }
+              }else if(val>node.val){
+                     if(node.right===null){
+                        node.right = new TreeNode(val)
+                        return
+                     }else{
+                        return searchTree(node.right)
+                     }
+              }else{
+                  return null
+              }
+           }
+           return searchTree(node)
         }
-        return node.value
+
+    }
+    // dfs
+    inOrder(node){
+            if(node!==null){
+                this.inOrder(node.left) // 1 
+                console.log(node.val)
+                this.inOrder(node.right)
+            }
+       }
+    postOrder(node){
+        if(node!==null){
+            this.inOrder(node.left)
+            this.inOrder(node.right)
+            console.log(node.val)
+        }
+    }
+    preOrder(node){
+        if(node!==null){
+            console.log(node.val)
+            this.preOrder(node.left)
+            this.preOrder(node.right)
+        }
+    }
+    min(){
+        let curr = this.root
+        while(curr.left!==null){
+            curr = curr.left
+        }
+        console.log(curr.val)
     }
 
-    _max(node){
-        if(!node) return null
-        if(node.right){
-          return  this._max(node.right)
+    // search(value,node){  // recursion
+       
+    //     if(node===null) return false
+    //     if(value===node.val) return {status:true,value: node.val}
+    //     if(value<node.val){
+    //         return this.search(value,node.left)
+    //     }else if(value>node.val){
+    //         return this.search(value,node.right)
+    //     }
+    // }
+
+    search(value){
+        let node = this.root
+        while(node!==null){
+            if(node.val===value){
+                return true
+            }
+            if(value<node.val){
+                node = node.left
+            }else{
+                node = node.right
+            }
         }
-        return node.value
+        return false
     }
 
-    insert(value){
-        this.root = this.insert_recursion(this.root,value)
-        return
-    }
+    //TODO: bfs pending ..
 
-    insert_recursion(node,value){
-        if(!node) return new Node(value)
-        if(value<node.value){
-            node.left =  this.insert_recursion(node.left,value)
-        }else if(value>node.value){
-            node.right= this.insert_recursion(node.right,value)
-        }
-        return node
-    }
-    // traverse
-    // DFS
-    // pre-order
-    // in-order
-    in_order(node=this.root){
-        if(node){
-            this.in_order(node.left)
-            console.log(node.value)
-            this.in_order(node.right)
-        }
-    }
-    pre_order(node=this.root){
-        if(node){
-            console.log(node.value)
-            this.pre_order(node.left) 
-            this.pre_order(node.right) 
-        }
-    }
-    post_order(node=this.root){
-        if(node){
-            this.post_order(node.left)
-            this.post_order(node.right) 
-            console.log(node.value) 
-        }
-    }
+    // remove(value){
+    //     const removeNode = (node,value)=>{
+    //         if(node===null){
+    //             return null;
+    //         }
+    //         if(node.val===value){
+    //               //  leaf (no children)
+    //           if(node.left === null && node.right === null){
+    //             return null
+    //           }
+    //           // single child left or right node
+    //           if(node.left===null){
+    //               return node.right
+    //           }
+    //           if(node.right===null){
+    //             return node.left
+    //           }
+    //           // two children node
+    //           // find the successor
+    //           let tempNode = node.right
+    //           while(tempNode.left!==null){
+    //             tempNode= tempNode.left
+    //           }
+    //           node.val=tempNode.val
+    //           node.right = removeNode(node.right,tempNode.val)
+    //           return node;
+    //         }else if(value<node.val){
+    //           node.left =  removeNode(node.left,value)
+    //           return node
+    //         }else{
+    //             node.right =  removeNode(node.right,value)
+    //             return node
+    //         }
+    //     }
+    //     this.root =  removeNode(this.root,value) 
+    // }
 
-    // bfs
+    remove(value){
+        this.root = removeNode(this.root,value)
+        function removeNode(node,value){
+            if(node===null){
+                return null
+            }
+            if(value===node.val){
+                if(node.left===null && node.right===null){
+                    return null
+                }
+                if(node.left===null){
+                    return node.right
+                }
+                if(node.right===null){
+                    return node.left
+                }
+                let successor = node.right
+                while(successor.left!==null){
+                    successor = successor.left
+                }
+                node.val= successor.val
+                // delete duplicate successor
+                node.right = removeNode(node.right,successor.val)
+                return node
+            }else if(value<node.val){
+                node.left =  removeNode(node.left,value)
+                return node
+            }else{
+                // greater the node value
+                node.right = removeNode(node.right,value)
+                return node.right
+            }
+        
+        }
+    }
 
     bfs(){
         const queue = []
         const result = []
-        if(!this.root) return null
-        queue.push(this.root)
+        let curr = this.root
+        queue.push(curr)
         while(queue.length>0){
-           const cur_node = queue.shift();
-           result.push(cur_node.value)
-           if(cur_node.left){
-              queue.push(cur_node.left)
-           } 
-           if(cur_node.right){
-             queue.push(cur_node.right)
-           }
+            const curr= queue.shift()
+            result.push(curr.val)
+            if(curr.left) queue.push(curr.left)
+            if(curr.right) queue.push(curr.right)
         }
-        return result
+     return result
     }
 
-    delete(value){
-        if(value===undefined) return null
-        this.root=this.delete_recursion(this.root,value)
-    }
-    delete_recursion(node,value){
-        if(!node) return null
-        if(value<node.value){
-           node.left  = this.delete_recursion(node.left) 
-        }else if(value>node.value){
-          node.right=  this.delete_recursion(node.right)
-        }else{
-            if(!node.left&&!node.right){ // leaf node
-                return null
-            }else if(!node.left){ // 1 children
-                return node.right
-            }else if(!node.right){
-                return node.left
-            }
-
-            // 2 children
-            const min_value = this._min(node.right)
-            node.value= min_value
-            node.right = this.delete_recursion(node.right,min_value)
-        }
-        return node
-    }
 
     
-
-
-
 }
 
 const bst = new BST()
-bst.insert(100)
-bst.insert(400)
-bst.insert(10)
-bst.insert(-1)
-
-// bst.delete(400)
-// bst.in_order()
-// console.log(".......")
-// bst.pre_order()
-// console.log(".......")
-// bst.post_order()
+bst.add(2)
+bst.add(100)
+bst.add(-3)
+bst.add(40)
+// bst.preOrder(bst.root)
+// console.log(bst)
+// console.log(bst.search(-3,bst.root))
+// console.log(bst.search(-0.3))
+// console.log(bst.remove(2))
+// bst.preOrder(bst.root)
 console.log(bst.bfs())
