9832459a538b4358f314f64b317afe989a72b7d7
sivin-s <sivin9454@gmail.com>
2025-09-04 16:07:51 +0530

    add useReducer

---
+++ react/react_practical_IM/useReducer/reducer_example2/useReducer_eg2/src/App.jsx
@@ -0,0 +1,45 @@
+import {useReducer} from 'react'
+
+function reducer(state, action){
+  console.log(action)
+   switch(action.type){
+     case 'increment':
+      return {...state, value: state.value + action.payload}
+     case 'decrement':
+      return {...state, value:state.value + action.payload}
+     case 'color':
+       return {...state, color: action.payload }
+     default:
+      return state
+   }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer,{
+    value: 0
+  })
+  console.log(state)
+  return(<>
+      <h1 style={{color: state.color ? state.color : ''}}>{state.value}</h1>
+      <button onClick={(e)=>{
+        e.preventDefault()
+        dispatch({type:'increment',payload:1})
+      }}>Increment</button>
+      <button onClick={(e)=>{
+        e.preventDefault()
+        dispatch({type:'decrement', payload:-1})
+      }}>Decrement</button>
+      <button onClick={(e)=>{
+         e.preventDefault()
+         dispatch({type:'color',payload:'red'})
+      }}>Red</button>
+      <button onClick={(e)=>{
+         e.preventDefault()
+         dispatch({type: 'color', payload:'orange'})
+      }}>
+       Orange
+      </button>
+  </>)
+}
+
+export default App
